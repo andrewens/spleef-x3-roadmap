f90484975a0c498e832678fe86e3d77ec99d3ad9
andrewens <andrewens4@gmail.com>
2025-01-14 13:41:29 -0700

    Roadmap for Spleef X3

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const LOCK_ICON = "assets/lock.png";
 const UNKNOWN_ICON = "assets/question-mark.png";
 const IN_PROGRESS_ICON = "assets/in-progress.png";
 
-const UNKNOWN_INDEX = 2; // completionIndex at which nodes are marked unknown instead of just locked
+const UNKNOWN_INDEX = 20; // completionIndex at which nodes are marked unknown instead of just locked
 // completionIndex=0 for nodes that are completed; completionIndex=1 for nodes that have a completed dependency; etc etc
-const FORCE_DISPLAY_ICONS = false; // set to false for lock icons
+const FORCE_DISPLAY_ICONS = true; // set to false for lock icons
 
 const BIG_NUMBER = 10 ^ 6; // completion index can't be bigger than this... idk how javascript works yet lol
 
